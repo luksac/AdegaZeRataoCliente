c8916eb5504623163f16cd2a281f1d7c7c72419e
Paulo Victor Almeida <pvaalmeida.a@outlook.com>
2020-07-01 13:42:58 -0300

    Atualizando cadastro

--- AdegaZeRataoCliente/src/pages/Criacao.js
+++ AdegaZeRataoCliente/src/pages/Criacao.js
@@ -2,21 +2,17 @@ import React, { Component } from 'react';
 import Rato from '../components/images/2.jpg';
 import { Link } from 'react-router-dom';
 import CervejaService from '../service/cervejaService.js';
-import DetalhesService from '../service/detalhesService.js';
 import AuthService from '../service/authService.js';
-import { ButtonToolbar, Button } from 'react-bootstrap';
-import { Modal, Row, Col, Form } from 'react-bootstrap';
 
 class Criacao extends Component {
     constructor(props) {
         super(props);
         this.state = {
             idCerveja: '',
+            idDetalhes: '',
             Descricao: '',
-            Detalhes: '',
             Categoria: '',
             Tipo: '',
-            Preco: '',
             Foto: '',
             error: 'Erro ao Inserir Registro, Tente Novamente mais tarde.',
             error_status: false,
@@ -32,47 +28,27 @@ class Criacao extends Component {
         e.preventDefault();
         var objetoCerveja = JSON.stringify({
             idCerveja: parseInt(this.state.idCerveja, 10),
-            idDetalhes: parseInt(this.state.idCerveja, 10),
+            idDetalhes: parseInt(this.state.idDetalhes, 10),
             descricao: this.state.Descricao,
             categoria: this.state.Categoria,
             tipo: this.state.Tipo,
             foto: this.state.Foto
 
         })
-        var objetoDetalhes = JSON.stringify({
-            idDetalhes: parseInt(this.state.idCerveja, 10),
-            Ranking: parseInt(this.state.idCerveja, 10),
-            ProdutoQuantidade: 20,
-            PrecoSugerido: parseFloat(this.state.Preco),
-            DescricaoDetalhes: this.state.Detalhes
-
-        })
         AuthService.Token()
             .then((res) => res)
             .then((response) => {
                 console.log('AQUI', response.tokenizer);
-                this.setState({ sucess_stats: true })
-                DetalhesService.Salvar(objetoDetalhes, response.tokenizer)
-                    .then((res2) => res2)
-                    .then((response2) => {
-                        console.log('AQUI', response2);
-                        if (response2.status == 400) {
+                CervejaService.Salvar(objetoCerveja, response.tokenizer)
+                    .then((res) => res)
+                    .then((response) => {
+                        console.log('AQUI', response);
+                        if (response.status == 400) {
                             this.setState({ error_status: true })
                         } else {
                             this.setState({ sucess_stats: true })
-                            CervejaService.Salvar(objetoCerveja, response.tokenizer)
-                                .then((res) => res)
-                                .then((response) => {
-                                    console.log('AQUI', response);
-                                    if (response.status == 400) {
-                                        this.setState({ error_status: true })
-                                    } else {
-
-                                    }
-                                }).catch(error => console.log('CervejaError: ', error));
                         }
-                    }).catch(error => console.log('DetalhesError: ', error));
-
+                    }).catch(error => console.log('CervejaError: ', error));
             }).catch(error => console.log('AuthError: ', error));
 
     }
@@ -121,7 +97,7 @@ class Criacao extends Component {
                             <div class="alert alert-success alert-dismissible" id="mensagem-erro">
                                 <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
                                 <strong>Erro!</strong> Registro Criado Com Sucesso!
-                             </div>
+                </div>
                             :
                             <div>
                             </div>
@@ -131,7 +107,7 @@ class Criacao extends Component {
                             <div class="alert alert-danger alert-dismissible" id="mensagem-erro">
                                 <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
                                 <strong>Erro!</strong> Tente Novamente Mais Tarde ou Entre em Contato com o Suporte.
-                            </div>
+        </div>
                             :
                             <div>
                             </div>
@@ -151,11 +127,12 @@ class Criacao extends Component {
                                     </div>
 
                                     <div className="input-group2">
-                                        <input className="input--style-2" type="text" placeholder="Nome da Cerveja" name="Descricao" value={this.state.Descricao} onChange={this.changeHandler}></input>
+                                        <input className="input--style-2" type="text" placeholder="Codigo Detalhes" name="idDetalhes" value={this.state.idDetalhes} onChange={this.changeHandler}></input>
                                     </div>
 
+
                                     <div className="input-group2">
-                                        <input className="input--style-2" type="text" placeholder="Detalhes" name="Detalhes" value={this.state.Detalhes} onChange={this.changeHandler}></input>
+                                        <input className="input--style-2" type="text" placeholder="Descrição" name="Descricao" value={this.state.Descricao} onChange={this.changeHandler}></input>
                                     </div>
 
                                     <div className="input-group2">
@@ -163,27 +140,16 @@ class Criacao extends Component {
                                     </div>
 
                                     <div className="input-group2">
-                                        {/* <input className="input--style-2" type="text" placeholder="Categoria" name="Categoria" value={this.state.Categoria} onChange={this.changeHandler}></input> */}
-                                        <label>Categoria: </label><br></br>
-                                        <select onChange={this.changeHandler} value={this.state.Categoria} name="Categoria">
-                                            <option value="Não Maltada">Não Popular</option>
-                                            <option value="Maltada">Popular</option>
-                                        </select>
+                                        <input className="input--style-2" type="text" placeholder="Categoria" name="Categoria" value={this.state.Categoria} onChange={this.changeHandler}></input>
                                     </div>
 
-                                    <div className="input-group2">
-                                        <input className="input--style-2" type="text" placeholder="Preço Sugerido" name="Preco" value={this.state.Preco} onChange={this.changeHandler}></input>
-                                    </div>
 
                                     <div className="input-group2">
                                         <input className="input--style-2" type="text" placeholder="IMAGEM" name="Foto" value={this.state.Foto} onChange={this.changeHandler}></input>
                                     </div>
 
                                     <div className="">
-                                        <ButtonToolbar>
-                                            <Button className="button-Criar button-Criar-espace" type="submit" >Salvar</Button>
-                                            <Link to={'/'} ><Button variant='primary' className="button-Criar">Voltar</Button></Link>
-                                        </ButtonToolbar>
+                                        <button className="button-Criar" type="submit">Cadastrar</button>
                                     </div>
                                 </form>
                             </div>
